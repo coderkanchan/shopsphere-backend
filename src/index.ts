dbf12a3619bfda5d1601a749078622e25e0836cc
kanchan sharma <kanusharma881388@gmail.com>
2026-07-30 21:11:29 +0530

    learn: understand Prisma comparison filters gt and gte

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
-
   const users = await prisma.user.findMany({
     where: {
       name: {
-        contains: "an"
+        startsWith: "A"
       }
     }
   });
