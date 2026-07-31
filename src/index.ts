23c1b724c6921c1cef14ce35856c4aa925787ec1
kanchan sharma <kanusharma881388@gmail.com>
2026-07-31 19:33:51 +0530

    feat(prisma): implement nested create with user and posts

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,29 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: 1
+  const user = await prisma.user.create({
+    data: {
+      name: "Rahul",
+      email: "rahul@gmail.com",
+      age: 25,
+
+      posts: {
+        create: [
+          {
+            title: "My First Post",
+            content: "Learning Prisma is awesome!",
+          },
+          {
+            title: "Second Post",
+            content: "Nested create is powerful.",
+          },
+        ],
+      },
     },
+
     include: {
-      orders: true
-    }
+      posts: true,
+    },
   });
 
   console.log(user);
