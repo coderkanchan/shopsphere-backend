e2f3f2ba088945dec9b10c2214b9822db094ab2f
kanchan sharma <kanusharma881388@gmail.com>
2026-08-11 19:17:01 +0530

    learning upsert

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,18 @@ import prisma from "./config/prisma";
 async function main() {
   const user = await prisma.user.upsert({
     where: {
-      email: "priya@gmail.com",
+      email: "ananya@gmail.com",
     },
-
     update: {
-      age: 23,
+      age: 21,
     },
-
     create: {
-      name: "Priya",
-      email: "priya@gmail.com",
-      age: 23,
+      name: "Ananya",
+      email: "ananya@gmail.com",
+      age: 21,
     },
   });
-  
+
   console.log(user);
 }
 main()
