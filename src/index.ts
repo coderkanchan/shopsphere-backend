841c1b2c63513f6009e49d5b2dcd617de29ece54
kanchan sharma <kanusharma881388@gmail.com>
2026-08-11 23:23:49 +0530

    learned prisma deleteMany

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,40 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
-  const result = await prisma.user.createMany({
-    data: [
-      {
-        name: "Aarav",
-        email: "aarav@gmail.com",
-        age: 24,
+  // const result = await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       name: "Aarav",
+  //       email: "aarav@gmail.com",
+  //       age: 24,
+  //     },
+  //     {
+  //       name: "Neha",
+  //       email: "neha@gmail.com",
+  //       age: 26,
+  //     },
+  //     {
+  //       name: "Ishita",
+  //       email: "ishita@gmail.com",
+  //       age: 23,
+  //     },
+  //   ],
+  // });
+
+  const result = await prisma.user.updateMany({
+    where: {
+      age: {
+        gte: 25,
       },
-      {
-        name: "Neha",
-        email: "neha@gmail.com",
-        age: 26,
-      },
-      {
-        name: "Ishita",
-        email: "ishita@gmail.com",
-        age: 23,
-      },
-    ],
+    },
+    data: {
+      age: 30,
+    },
   });
 
   console.log(result);
+
+
 }
 
 main()
