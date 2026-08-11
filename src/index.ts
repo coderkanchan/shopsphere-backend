96b43660516bbf2eed83f4b744a9dc2a52f95c54
kanchan sharma <kanusharma881388@gmail.com>
2026-08-11 23:24:34 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,6 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
-  // const result = await prisma.user.createMany({
-  //   data: [
-  //     {
-  //       name: "Aarav",
-  //       email: "aarav@gmail.com",
-  //       age: 24,
-  //     },
-  //     {
-  //       name: "Neha",
-  //       email: "neha@gmail.com",
-  //       age: 26,
-  //     },
-  //     {
-  //       name: "Ishita",
-  //       email: "ishita@gmail.com",
-  //       age: 23,
-  //     },
-  //   ],
-  // });
 
   const result = await prisma.user.updateMany({
     where: {
