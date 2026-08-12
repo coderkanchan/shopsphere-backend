dd50a07014a21a18de2523a52aec9a2276b1fc55
kanchan sharma <kanusharma881388@gmail.com>
2026-08-12 21:26:01 +0530

    learn Prisma transactions

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,23 @@ import prisma from "./config/prisma";
 
 async function main() {
 
-  const result = await prisma.user.groupBy({
-    by: ["age"],
-    _count: {
-      _all: true,
-    },
-  });
+  const result = await prisma.$transaction([
+    prisma.user.create({
+      data: {
+        name: "Ananya",
+        email: "ananya@gmail.com",
+        age: 24,
+      },
+    }),
 
-  
+    prisma.user.create({
+      data: {
+        name: "Kavya",
+        email: "kavya@gmail.com",
+        age: 26,
+      },
+    }),
+  ]);
 }
 
 main()
