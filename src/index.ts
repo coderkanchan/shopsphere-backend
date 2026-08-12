ed32e77820b90471e352a738d465b8e32a10383e
kanchan sharma <kanusharma881388@gmail.com>
2026-08-12 18:46:56 +0530

    learn prisma Aggregate avg

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,9 @@ import prisma from "./config/prisma";
 
 async function main() {
 
-  const posts = await prisma.post.findMany({
-    where: {
-      user: {
-        age: {
-          gt: 25,
-        },
-      },
-    },
-  });
-
-  console.log(posts);
-
-  const results = await prisma.user.count();
-
-  const result = await prisma.user.count({
-    where: {
-      age: {
-        gte: 18,
-      },
+  const result = await prisma.user.aggregate({
+    _avg: {
+      age: true,
     },
   });
 
