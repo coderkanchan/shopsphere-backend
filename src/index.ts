ccf5d327ef14ce9aa47d856d03afd7c4bbcc141c
kanchan sharma <kanusharma881388@gmail.com>
2026-08-19 13:13:17 +0530

    learn SQL subqueries and exists

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,22 @@ import prisma from "./config/prisma";
 import { Prisma } from "./generated/prisma/client";
 
 async function main() {
-  try {
-    await prisma.user.create({
-      data: {
-        name: "Another User",
-        email: "ananya@gmail.com",
-        age: 25,
-      },
-    });
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log(error.code);
-    }
-  }
-  const ages = await prisma.user.findMany({
-    distinct: ["age"],
-    select: {
+  const result = await prisma.user.aggregate({
+    _avg: {
       age: true,
     },
   });
 
-  const result = await prisma.$queryRaw`
-  SELECT *
-  FROM "Color"
-  CROSS JOIN "Size";
-`;
+  const averageAge = result._avg.age;
 
-  const employees = await prisma.employee.findMany({
-    include: {
-      manager: true,
+  const users = await prisma.user.findMany({
+    where: {
+      age: {
+        gt: averageAge ?? undefined,
+      },
     },
   });
-  
+
 }
 
 main()
