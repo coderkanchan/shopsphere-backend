90a49d0691b3826fda0328e5287ab89899884bb6
kanchan sharma <kanusharma881388@gmail.com>
2026-08-20 18:48:05 +0530

    learn SQL subqueries and exists

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,7 @@ import prisma from "./config/prisma";
 import { Prisma } from "./generated/prisma/client";
 
 async function main() {
-  const result = await prisma.user.aggregate({
-    _avg: {
-      age: true,
-    },
-  });
-
-  const averageAge = result._avg.age;
-
-  const users = await prisma.user.findMany({
-    where: {
-      age: {
-        gt: averageAge ?? undefined,
-      },
-    },
-  });
+ 
 
 }
 
