2c1176c5cb710682c18f31379565d4c6efe7cd89
kanchan sharma <kanusharma881388@gmail.com>
2026-08-25 23:28:45 +0530

    learn PostgreSQL date range filtering

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import prisma from "./config/prisma";
 import { Prisma } from "./generated/prisma/client";
 
 async function main() {
+  const startDate = new Date("2026-08-01");
+  const endDate = new Date("2026-09-01");
+
   const users = await prisma.user.findMany({
     where: {
       createdAt: {
-        gte: new Date("2026-08-01"),
+        gte: startDate,
+        lt: endDate,
       },
     },
   });
 }
-  main()
-    .catch(console.error)
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
+main()
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
