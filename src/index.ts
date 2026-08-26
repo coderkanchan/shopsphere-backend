33b8fc12804accb968ad9713be6c5d08ea030d81
kanchan sharma <kanusharma881388@gmail.com>
2026-08-26 16:53:00 +0530

    learn PostgreSQL string functions

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,11 @@ import prisma from "./config/prisma";
 import { Prisma } from "./generated/prisma/client";
 
 async function main() {
-  const startDate = new Date("2026-08-01");
-  const endDate = new Date("2026-09-01");
-
   const users = await prisma.user.findMany({
     where: {
-      createdAt: {
-        gte: startDate,
-        lt: endDate,
+      name: {
+        contains: "kan",
+        mode: "insensitive",
       },
     },
   });
