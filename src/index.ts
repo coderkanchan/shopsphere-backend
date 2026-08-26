860a78549a77b1152608fbe33174ee28224b8d25
kanchan sharma <kanusharma881388@gmail.com>
2026-08-26 18:02:00 +0530

    learn SQL case null handling and type casting

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import prisma from "./config/prisma";
 import { Prisma } from "./generated/prisma/client";
 
 async function main() {
-  const users = await prisma.user.findMany({
-    where: {
-      name: {
-        startsWith: "Kan",
-        mode: "insensitive",
-      },
-    },
-  });
+  const users = await prisma.$queryRaw`
+  SELECT
+    name,
+    age,
+    CASE
+      WHEN age < 18 THEN 'Minor'
+      WHEN age < 60 THEN 'Adult'
+      ELSE 'Senior'
+    END AS age_group
+  FROM "User";
+`;
 }
 
 main()
