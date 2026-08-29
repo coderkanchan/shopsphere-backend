d3ca094e819c77c61d4fc196896169e23507b46b
kanchan sharma <kanusharma881388@gmail.com>
2026-08-29 22:50:05 +0530

    learn SQL ranking window functions

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,13 @@ import { Prisma } from "./generated/prisma/client";
 
 async function main() {
   const result = await prisma.$queryRaw`
-  WITH ranked_posts AS (
-    SELECT
-      p.*,
-      ROW_NUMBER() OVER (
-        PARTITION BY "userId"
-        ORDER BY "createdAt" DESC
-      ) AS row_num
-    FROM "Post" p
-  )
-  SELECT *
-  FROM ranked_posts
-  WHERE row_num = 1;
+  SELECT
+    name,
+    spending,
+    RANK() OVER (
+      ORDER BY spending DESC
+    ) AS rank
+  FROM "User";
 `;
 }
 
