8f480c781b320d23103a007282d9d6dfb46d3b8f
kanchan sharma <kanusharma881388@gmail.com>
2026-08-30 23:43:37 +0530

    learn PostgreSQL keys and constraints

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import prisma from "./config/prisma";
 import { Prisma } from "./generated/prisma/client";
 
 async function main() {
- 
+ CREATE TABLE "Post" (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  "userId" INT,
+  FOREIGN KEY ("userId")
+    REFERENCES "User"(id)
+);
 }
 
 main()
   .catch(console.error)
   .finally(async () => {
     await prisma.$disconnect();
-  });
+  }); 
