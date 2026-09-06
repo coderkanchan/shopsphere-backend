dc8097fc1072aeede316588bb66a3b93b9610cd0
kanchan sharma <kanusharma881388@gmail.com>
2026-09-06 22:46:02 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,6 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
-const result = await prisma.$queryRaw`
-  SELECT
-    name,
-    spending,
-    FIRST_VALUE(spending) OVER (
-      ORDER BY spending DESC
-    ) AS highest_spending
-  FROM "User";
-`;
 
 }
 
