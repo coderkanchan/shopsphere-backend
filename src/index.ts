4bfb3964bf34dd7514b346571f79ff117231cdb9
kanchan sharma <kanusharma881388@gmail.com>
2026-09-09 23:15:56 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,7 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
-  const result = await prisma.$transaction(async (tx) => {
-    const order = await tx.order.create({
-      data: {
-        userId: 1,
-        status: "PENDING",
-      },
-    });
-
-    const item = await prisma.user.create({
-      data: {
-        name: "Kanchan",
-        email: "kanchan@example.com",
-        posts: {
-          create: [
-            { title: "First Post" },
-            { title: "Second Post" }
-          ]
-        }
-      }
-    });
-    await tx.product.update({
-      where: {
-        id: 10,
-      },
-      data: {
-        stock: {
-          decrement: 2,
-        },
-      },
-    });
-
-    return order;
-  });
+ 
 }
 
 main()
