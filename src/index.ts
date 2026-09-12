8a103747a0fd8b199f4183a4eb59d47bacb01d33
kanchan sharma <kanusharma881388@gmail.com>
2026-09-12 17:16:21 +0530

    learn PostgreSQL isolation and concurrency

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
- 
+  await prisma.$queryRaw`
+  SELECT *
+  FROM "Product"
+  WHERE id = 10
+  FOR UPDATE
+`;
 }
 
 main()
