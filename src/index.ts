2a6e732dd360c725aa2844a8583e822949057322
kanchan sharma <kanusharma881388@gmail.com>
2026-09-14 22:18:14 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import "dotenv/config";
 import prisma from "./config/prisma";
 
 async function main() {
-
+await prisma.$transaction(async (tx) => {
+  // order create
+  // order items create
+  // stock update
+});
 }
 
 main()
